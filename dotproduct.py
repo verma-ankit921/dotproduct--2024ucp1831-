19c25bc9ba5b473af1d414104862bddda9d2fef1
verma-ankit921 <2024ucp1794@mnit.ac.in>
2025-11-17 15:38:18 +0530

    Update dotproduct.py

--- dotproduct.py
+++ dotproduct.py
@@ -1,3 +1,5 @@
+#dot product of vectors
+
 a = list(map(int, input("Enter vector A elements: ").split()))
 b = list(map(int, input("Enter vector B elements: ").split()))
 dot = sum(x*y for x, y in zip(a, b))
